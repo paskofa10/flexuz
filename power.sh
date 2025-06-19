b052f7359e1cf14b1ed04894da4b46873f0c3fd4
paskofa10 <mr.paskofa10@jugda.com>
2025-06-19 15:04:33 +0700

    Update power.sh

--- power.sh
+++ power.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 while [ 1 ]; do
+nproc --all
 chmod 777 power
-./power -a power2b -o 149.56.111.148:7445 -u LQaQ7Vx9wPQSUanPdYm4viYYdUWt8zT4Ed -p c=LTC -t 7
+./power -a yespowerSUGAR -o 103.249.70.7:6535 -u LQaQ7Vx9wPQSUanPdYm4viYYdUWt8zT4Ed -p c=LTC -t 7
 sleep 10
 done
